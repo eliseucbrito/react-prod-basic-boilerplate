b495e83459ced74407eb30714e0defc5befa8a32
eliseucbrito <ecb2@cin.ufpe.br>
2025-01-17 11:54:37 -0300

    structure: update structure with Context template

---
+++ src/contexts/TemplateContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState, ReactNode, useMemo } from "react";
+
+interface TemplateContextProps {
+  value: number;
+  handleSetValue: (num: number) => void;
+}
+
+const TemplateContext = createContext<TemplateContextProps | undefined>(
+  undefined
+);
+
+interface TemplateProviderProps {
+  children: ReactNode;
+}
+
+export function TemplateContextProvider({ children }: TemplateProviderProps) {
+  const [value, setValue] = useState<number>(0);
+
+  function handleSetValue(num: number) {
+    setValue(num);
+  }
+
+  const contextValue = useMemo<TemplateContextProps>(
+    () => ({
+      value,
+      handleSetValue,
+    }),
+
+    [value]
+  );
+
+  return (
+    <TemplateContext.Provider value={contextValue}>
+      {children}
+    </TemplateContext.Provider>
+  );
+}
+
+export const TemplategesContext = (): TemplateContextProps => {
+  const context = useContext(TemplateContext);
+  if (!context) {
+    throw new Error("useTemplate must be used within a TemplateProvider");
+  }
+  return context;
+};
